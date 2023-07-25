082bb6a49b5f652d2d2cf022eecc4ea1174169c0
BZ <zhangzishan119@gmail.com>
2023-07-25 23:37:59 +1200

    Updated

---
+++ server/routes/posts.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/post.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/", verifyToken, getFeedPosts);
+
+/* UPDATE */
