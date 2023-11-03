e9a755ab492070d5056e6c539e1081be91f0316e
Bella Zhang <zhangzishan119@gmail.com>
2023-11-03 16:30:30 +1300

    Installed packages for client
    +Packages for front end
    +Folder structure setup
    +Themes setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authRecuver from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import {
+  presistStore,
+  PresistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PRESIST,
+  PURGE,
+  REGISTER
+} from "redux-presist";
+import storage from "redux-presist/lib/storage";
+import { PresistGate } from "redux-presist/integration/react";
+
+const presistConfig = { key: "root", storage, version: 1};
+const presistedReducer = presistedReducer(presistConfig, authReducer);
+const store = configStore({
+  reducer: presistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    specializableCheck: {
+      ignoreActions: [
+        FLUSH,
+        REHYDRATE,
+        PAUSE,
+        PRESIST,
+        PURGE,
+        REGISTER
+      ],
+    },
+  }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PresistGate loading={null} presistor={persistStore(store)}>
+      <App />
+      </PresistGate>
+    </Provider>
   </React.StrictMode>
 );
